ce749172eefd31fe7b54cad4e02bbf4c4876d795
sadia11-a <sadiasorna96@gmail.com>
2023-11-28 17:26:22 +0600

    represent all the course in table form in the admin dashboard

---
+++ src/hooks/useCourse.jsx
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "./useAxiosPublic";
+
+const useCourse = () => {
+  const axiosPublic = useAxiosPublic();
+
+  const {
+    data: course = [],
+    isPending: loading,
+    refetch,
+  } = useQuery({
+    queryKey: ["course"],
+    queryFn: async () => {
+      const res = await axiosPublic.get("/course");
+      return res.data;
+    },
+  });
+
+  return [course, loading, refetch];
+};
+
+export default useCourse;
